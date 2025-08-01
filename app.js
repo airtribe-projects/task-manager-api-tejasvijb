0834403c254019e30bbd08f2349ec79c454e8e78
tejasvijb <jbtejasvi@gmail.com>
2025-08-01 20:05:10 +0530

    fixes

--- app.js
+++ app.js
@@ -1,44 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 const { tasks } = require("./task.json");
+const { checkTaskExists, validateTaskInput } = require("./utils");
 module.exports = app;
+
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-function checkTaskExists(req, res, next) {
-    const id = parseInt(req.params.id);
-    const index = tasks.findIndex((task) => task.id === id);
-
-    if (index === -1) {
-        return res.status(404).send({ error: "Task not found" });
-    }
-
-    req.taskIndex = index;
-    req.task = tasks[index];
-    next();
-}
-
-function validateTaskInput(req, res, next) {
-    const { title, description, completed, priority } = req.body;
-
-    if (
-        // check priority low, medium, high
-        typeof title !== "string" ||
-        title.trim() === "" ||
-        typeof description !== "string" ||
-        description.trim() === "" ||
-        typeof completed !== "boolean" ||
-        (priority !== "low" && priority !== "medium" && priority !== "high")
-    ) {
-        return res.status(400).send({
-            error: "Invalid task input. 'title', 'priority' and 'description' must be non-empty strings, and 'completed' must be a boolean.",
-        });
-    }
-
-    next();
-}
 
 app.route("/tasks/:id")
     .get(checkTaskExists, (req, res) => {
@@ -46,7 +15,13 @@ app.route("/tasks/:id")
     })
     .put(checkTaskExists, validateTaskInput, (req, res) => {
         const { title, description, completed, priority } = req.body;
-        updatedTask = { ...req.task, title, description, completed, priority };
+        const updatedTask = {
+            ...req.task,
+            title,
+            description,
+            completed,
+            priority,
+        };
 
         tasks[req.taskIndex] = updatedTask;
         res.send(updatedTask);
